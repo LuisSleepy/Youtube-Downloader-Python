7210633d88c0fcaaa6e8d128b3a495be635873d9
Jan Luis Antoc <49801234+janLuisAntoc@users.noreply.github.com>
2020-06-09 14:00:02 +0800

    Downloading a video is already possible. More updates on the GUI of the application.

--- downloader/YTDownloader.py
+++ downloader/YTDownloader.py
@@ -9,14 +9,11 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
 from pytube import YouTube
+import time
 
 
 class Window:
-    entry_error = Label
-
     def __init__(self, master=None):
-        self.title = None
-
         self.master = master
         master.title("YouTube Video Downloader")
         # For the main label
@@ -37,45 +34,63 @@ class Window:
         self.youtubeEntry = Entry(self.master, width=50,
                                   textvariable=entry_var)
 
-        # For checking the video
+        # A button for checking the video
         self.check_button = Button(self.master, width=5, bg="blue", fg="white",
                                    text="Check", font=("Arial", 10, "bold"), command=self.check_link)
         self.check_button.grid(row=3, pady=10)
 
+        # Displays the possible error upon checking the video
         self.entry_error = Label(self.master, text="", fg="red", font=("Helvetica", 15))
         self.entry_error.grid(pady=(0, 30))
 
+        # Label for displaying the title of the video
         self.title_label = Label(self.master, fg="black", font=("Sans Serif", 20))
 
+        # Label for asking the confirmation from the user
         self.confirmation_label = Label(self.master, fg="black", font=("Sans Serif", 20))
 
+        # Frame for the yes and no button for confirmation of the video
         self.confirmation_frame = Frame(self.master)
 
+        # For the objects in the initial screen
+        self.title = None
         self.yes_button = Button()
         self.no_button = Button()
+
+        # For the objects in the second screen
         self.link = None
         self.to_init_screen_button = Button()
         self.choosing_dir_button = Button()
         self.directory_label = Label()
         self.proceed_button = Button()
+
+        # For the objects in the third screen
         self.res_choices = ttk.Combobox()
-        self.to_sec_screen_button = Button()
+        self.to_dir_screen_button = Button()
         self.res_label = Label()
         self.download_button = Button()
         self.download_label = Button()
+        self.video = None
 
+    # Controls the checking of the provided link for the video
+    # Some errors not yet shown to the user:
+        # 1. Empty or no link provided
     def check_link(self):
         self.link = self.youtubeEntry.get()
-        # If an error occurs
         try:
             title = pytube.YouTube(self.link).title
             self.show_title(title)
             self.entry_error['text'] = ""
+
+        # If an error occurs
         except pytube.exceptions.RegexMatchError:
             self.entry_error['text'] = "Invalid link"
 
+    # Controls the displaying of title
     def show_title(self, title):
         self.title = title
+
+        # Not yet clear but this might be an error that occurred once
         if self.title == "YouTube":
             self.entry_error['text'] = "Please try again"
             return
@@ -87,6 +102,7 @@ class Window:
         title_str = ""
         for j in title_list:
             title_str = title_str + j
+
         self.title_label['text'] = title_str
 
         self.confirmation_label['text'] = "Is this the video you want to download?"
@@ -112,46 +128,57 @@ class Window:
 
         self.confirmation_frame.grid(row=6)
 
+    # Controls the program after confirming the video to download
     def confirm_video(self):
+        # Removes any remnants of the initial screen
         self.reject_video()
         self.link_label.grid_forget()
         self.entry.grid_forget()
         self.check_button.grid_forget()
 
+        # Button for choosing the directory
         self.choosing_dir_button = Button(self.master, width=20, bg="green", fg="black",
                                           text="Choose Directory", font=("Arial", 20),
                                           command=self.choose_directory)
         self.choosing_dir_button.grid(row=2)
 
+        # Label for the chosen directory
         self.directory_label = Label(self.master, font=("Helvetica", 15))
         self.directory_label.grid(row=3)
 
+        # Button for going back to the initial screen
         self.to_init_screen_button = Button(self.master, width=10, bg="yellow", fg="black",
                                             text="Go Back", font=("Arial", 10),
                                             command=self.restart_init_screen)
         self.to_init_screen_button.grid(row=5)
 
+    # Goes back to the initial screen
     def restart_init_screen(self):
-        # Remove everything except those objects that should be seen in the initial screen
+        # Remove everything seen on the directory screen
         self.choosing_dir_button.grid_forget()
         self.directory_label.grid_forget()
         self.to_init_screen_button.grid_forget()
+
         self.link_label.grid(row=1)
         self.entry.grid(row=2)
         self.check_button.grid(row=3, pady=10)
         self.proceed_button.grid_forget()
 
+    # Clears mostly the objects in the second screen and applied to both confirming and rejecting
+    # the video
     def reject_video(self):
         # Delete the content in the field where the link will be pasted
         self.entry.delete(0, 'end')
 
-        # Delete the shown title, the confirmation label, and the two buttons
+        # Delete the title, confirmation text, and the yes and no buttons
         self.title_label.grid_forget()
         self.confirmation_label.grid_forget()
         self.yes_button.pack_forget()
         self.no_button.pack_forget()
 
+    # Controller for choosing the directory
     def choose_directory(self):
+        # Allows opening the file dialog
         folder_name = filedialog.askdirectory()
 
         if len(folder_name) > 1:
@@ -162,29 +189,36 @@ class Window:
         else:
             self.directory_label.config(text="Please choose a folder!", fg="red")
 
+    # Provides the proceed button going to the download screen
     def to_choose_res(self):
         self.proceed_button = Button(self.master, width=10, bg="green", fg="black",
                                      text="Proceed", font=("Arial", 10), command=self.choose_res)
         self.proceed_button.grid(row=4)
 
-    def restart_sec_screen(self):
+    # Goes back to the directory screen
+    def restart_dir_screen(self):
+        # Removes the content not seen on the directory screen
         self.res_label.grid_forget()
         self.res_choices.grid_forget()
         self.download_button.grid_forget()
-        self.to_sec_screen_button.grid_forget()
+        self.to_dir_screen_button.grid_forget()
+        self.download_label.grid_forget()
 
         self.choosing_dir_button.grid(row=2)
-        # self.to_init_screen_button.grid(row=5)
+        self.to_init_screen_button.grid(row=5)
 
+    # Displays the objects on the download screen
     def choose_res(self):
         self.choosing_dir_button.grid_forget()
         self.directory_label.grid_forget()
         self.proceed_button.grid_forget()
+        self.to_init_screen_button.grid_forget()
 
-        video = YouTube(self.link)
+        self.video = YouTube(self.link)
 
+        # Gets the available video resolution and display these through a Combo Box
         res_dict = dict()
-        for stream in video.streams.filter(progressive=True):
+        for stream in self.video.streams.filter(progressive=True):
             res_dict[stream.itag] = stream.resolution
         res_values_list = list(res_dict.values())
 
@@ -199,24 +233,32 @@ class Window:
                                       command=self.download)
         self.download_button.grid(row=4)
 
-        self.to_sec_screen_button = Button(self.master, width=10, bg="yellow", fg="black",
-                                           text="Go Back", font=("Arial", 10),
-                                           command=self.restart_sec_screen)
-        self.to_sec_screen_button.grid(row=5)
+        self.download_label = Label(self.master, fg="green", font=("Sans Serif", 15))
+        self.download_label.grid(row=5)
 
+        self.to_dir_screen_button = Button(self.master, width=10, bg="yellow", fg="black",
+                                           text="Go Back", font=("Arial", 10),
+                                           command=self.restart_dir_screen)
+        self.to_dir_screen_button.grid(row=6, pady=15)
+
+    # Conducts the downloading of the video
     def download(self):
         choice = self.res_choices.get()
         if choice is "":
-            self.download_label = Label(self.master, fg="green", text="Choose a resolution",
-                                        font=("Sans Serif", 20))
-            self.download_label.grid(row=5)
+            self.download_label['text'] = "No resolution selected!"
         else:
-            self.download_label = Label(self.master, fg="green", text=choice + " video is downloading",
-                                        font=("Sans Serif", 20))
-            self.download_label.grid(row=5)
+            chosen_res = self.video.streams.get_by_resolution(choice)
+            chosen_res.download(self.directory_label['text'])
+            self.download_label['text'] = "Finished download"
 
+            time.sleep(10)
+            self.master.destroy()
+            start()
 
-root = Tk()
-root.grid_columnconfigure(0, weight=1)
-downloader_GUI = Window(root)
-root.mainloop()
+def start():
+    root = Tk()
+    root.grid_columnconfigure(0, weight=1)
+    Window(root)
+    root.mainloop()
+
+start()
